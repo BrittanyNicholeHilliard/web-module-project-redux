f9b6a386f3d5c4ae8eafdf1a7ceea8317f3e7172
timothy hoover <bnicholehilliard@gmail.com>
2022-09-08 16:12:54 -0700

    first push, store created

--- src/index.js
+++ src/index.js
@@ -9,6 +9,14 @@ import App from './components/App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 
+
+let store
+export const resetStore = () => {
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+  store = createStore(reducer, composeEnhancers())
+}
+resetStore()
+
 ReactDOM.render(
   <Router>
       <App />
